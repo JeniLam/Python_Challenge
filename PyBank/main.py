3d61143b7954331fd6e459b6829f5300e3d18865
JeniLam <jenilam639@gmail.com>
2020-09-17 22:52:13 -0500

    Adds starter code and resources to Poll

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,6 +1,22 @@
-# Your task is to create a Python script that analyzes the records to calculate each of the following:
-#The total number of months included in the dataset
-#The net total amount of "Profit/Losses" over the entire period
+import os
+import csv
+# Path to collect data from the Resources folder
+budget_data = os.path.join('..', 'Resources', 'budget_data.csv')
+
+# create function to 
+
+
+
+# The total number of months included in the dataset
+# The net total amount of "Profit/Losses" over the entire period
 # The average of the changes in "Profit/Losses" over the entire period
 # The greatest increase in profits (date and amount) over the entire period
 # The greatest decrease in losses (date and amount) over the entire period
+#The below will be commented out until I figure out the above
+print("Financial Analysis")
+print("---------------------------")
+print("Total Months: ") 
+print("Total: ")
+print("Average Change: ")
+print("Greatest Increase in Profits: ")
+print("Greatest Decrease in Profits: ")
---
+++ PyPoll/main.py
@@ -0,0 +1,17 @@
+import os
+import csv
+# Path to collect data from the Resources folder
+election_data = os.path.join('..', 'Resources', 'election_data.csv')
+# The total number of votes cast
+# A complete list of candidates who received votes
+# The percentage of votes each candidate won
+# The total number of votes each candidate won
+# The winner of the election based on popular vote.
+print("Election Results")
+print("---------------------------")
+print("Total Votes: ")
+print("---------------------------")
+#candidate dictionary or list goes here
+print("---------------------------")
+print("Winner: ")
+print("---------------------------")

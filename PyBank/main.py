f1e925a323e35a161a9734c6d2bc1691a00dd9f1
JeniLam <jenilam639@gmail.com>
2020-09-18 23:33:07 -0500

    Adds total votes and average P/L starter

--- PyBank/main.py
+++ PyBank/main.py
@@ -12,32 +12,34 @@ with open(budget_data) as csvfile:
     #store text in variable called bank
     bank = (csvreader)
     print(bank)
+
     #Begin output
     print("Financial Analysis")
     print("---------------------------")
+
     # The total number of months included in the dataset
     # counted number of lines in dataset minuus the header
     Totalmonths= len(list(bank))-1
     print("Total Months: " + str(Totalmonths))
+
+    # The net total amount of "Profit/Losses" over the entire period
+    #declare variable first
         
-
-
-# net_amount = 0
-# average_change = 0
-# greatest_increase = 0
-# greatest_decrease = 0
-
+    # for row in bank:
+    #     Total = sum(int(row[1])
+    #     #avgpl = int(Total/Totalmonths)
+    #     print('Total: $' + str(Total))
+    #     print("Average Change: $"+ str(avgpl))
 
 
 
-# The net total amount of "Profit/Losses" over the entire period
 # The average of the changes in "Profit/Losses" over the entire period
 # The greatest increase in profits (date and amount) over the entire period
 # The greatest decrease in losses (date and amount) over the entire period
 
 
 
-print("Total: ")
+
 print("Average Change: ")
 print("Greatest Increase in Profits: ")
 print("Greatest Decrease in Profits: ")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -12,14 +12,21 @@ with open(election_data) as csvfile:
     #store text in variable called poll data
     poll_data = (csvreader)
     print(poll_data)
-# The total number of votes cast
+
+    #Begin output
+    print("Election Results")
+    print("---------------------------")
+
+    # The total number of votes cast
+    # counted number of lines in dataset minuus the header
+    total_votes = len(list(poll_data))-1
+    print("Total Votes: " + str(total_votes))
 # A complete list of candidates who received votes
 # The percentage of votes each candidate won
 # The total number of votes each candidate won
 # The winner of the election based on popular vote.
-print("Election Results")
-print("---------------------------")
-print("Total Votes: ")
+
+
 print("---------------------------")
 #candidate dictionary or list goes here
 print("---------------------------")

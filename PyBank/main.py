9b723b5f4ec6c149aa498b483bf578adacb391e5
JeniLam <jenilam639@gmail.com>
2020-09-21 12:58:53 -0500

    Adds zip list to obtain max/min Profit Loss

--- PyBank/main.py
+++ PyBank/main.py
@@ -31,35 +31,37 @@ with open(budget_data) as csvfile:
         # turn date and pl column into list
         date.append(row[0])
         profitloss.append(row[1])
+        # zip appended lists together to create tuple to perform max/min features on
+        # research on these sites:https://stackoverflow.com/questions/7313157/python-create-list-of-tuples-from-lists/7313188
+        # https://www.tutorialspoint.com/python/tuple_max.htm#:~:text=Python%20tuple%20method%20max%20%28%29%20returns%20the,elements%20from%20the%20tuple%20with%20maximum%20value.    
+        ziplist = list(zip(date,profitloss))
         #counter for number of lines
         totalmonths += 1
         # how to sum csv column in python https://stackoverflow.com/questions/13517080/sum-a-csv-column-in-python
         totalPL += (int(row[1]))
     #got help from Wei on this equation below
     avgpl = (int(profitloss[0])- int(profitloss[85]))/85
-        # list/dictionary comprehension?
-        # greatestincrease = [max(row[0,1])]
-        # greatestdecrease = [min(row(0,10))]
+    # print(ziplist) not producing correct date/value??
+    greatestincrease = max(ziplist)
+    greatestdecrease = min(ziplist)
+       
     print("Total Months: " + str(totalmonths))    
     print('Total Net Profit/Losses: $' + str(totalPL))
     print("Average Change: $"+ str(avgpl))
-    # The greatest increase in profits (date and amount) over the entire period
-    # print("Greatest Increase in Profits: " + greatestincrease)
-    # # The greatest decrease in losses (date and amount) over the entire period
-    # #need Min
-    # print("Greatest Decrease in Profits: "+ greatestdecrease)
+    print("Greatest Increase in Profits: " + str(greatestincrease))
+    print("Greatest Decrease in Profits: "+ str(greatestdecrease))
 
 #write above to text file
 PyBank = output_path = os.path.join("analysis", "PyBank.txt")
-#\n creates new line
+#\n creates new line https://www.geeksforgeeks.org/reading-writing-text-files-python/#:~:text=Reading%20and%20Writing%20to%20text%20files%20in%20Python.,over-written.%20The%20handle%20is%20positioned%20...%20More%20items 
 with open(PyBank,'w') as file:
     file.write("Financial Analysis\n")
     file.write("---------------------\n")
     file.write("Total Months: %d\n" % totalmonths)
     file.write("Total Net Profit/Losses: $ %d\n" % totalPL)
     file.write("Average Change: %d\n" % avgpl)
-    file.write("Greatest Increase in Profits: $")
-    file.write("Greatest Decrease in Profits: $")
+    file.write("Greatest Increase in Profits:  %s ($%d)\n" % greatestincrease[0], greatestincrease[1])
+    file.write("Greatest Decrease in Profits:  %s ($%d)\n" % greatestdecrease[0], greatestdecrease[1])
    
 
 
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,20 +4,12 @@ import csv
 # Path to collect data from the Resources folder
 election_data = os.path.join('Resources', 'election_data.csv')
 
-#Begin print output
-print("Election Results")
-print("---------------------------")
 #define function for percentages and accept candidate_data as sole parameter
-
-def percentages(candidate_data):
-    #delare variables in candidate_data
-    votes = int(candidate_data[0])
-    candidates = str(candidate_data[2])
-   
-    
-    
-winner = 0
+# Lists to store data to then append
+candidates = []
+candidate_name = []
 totalvotes = 0
+winner = 0
 # open file and read, store contents as text
 with open(election_data) as csvfile:
     #store reference to file 
@@ -30,7 +22,9 @@ with open(election_data) as csvfile:
     # counted number of lines in dataset minuus the header
     for row in csvreader:
         totalvotes +=1
-
+    #Begin print output
+    print("Election Results")
+    print("---------------------------")
     print("Total Votes: " + str(totalvotes))
     print("---------------------------")
     # A complete list of candidates who received votes
